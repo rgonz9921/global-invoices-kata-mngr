f8c6c580261564cdd8811853409b9f01a893817c
rgonz9921 <rgonzs9921@gmail.com>
2026-09-02 16:18:22 -0500

    first commit :zap:

---
+++ src/main/java/com/project_kata/global_invoices_kata_mngr/GlobalInvoicesKataMngrApplication.java
@@ -0,0 +1,13 @@
+package com.project_kata.global_invoices_kata_mngr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalInvoicesKataMngrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalInvoicesKataMngrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project_kata/global_invoices_kata_mngr/GlobalInvoicesKataMngrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_kata.global_invoices_kata_mngr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalInvoicesKataMngrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
